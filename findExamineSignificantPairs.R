7e14712dffd65d52c552e4b29ac4b8083bb89d32
sastoudt <sstoudt@berkeley.edu>
2017-03-31 17:48:33 -0700

    redo analysis with fixed data

--- findExamineSignificantPairs.R
+++ findExamineSignificantPairs.R
@@ -11,30 +11,30 @@ names(withinStationDiffVarResults)=c("maxLag","pVal","minPval")
 
 ## first check to make sure we have availability to get significant p-values
 
-summary(acrossStationSameVarResults$minPval) ## 0.005848 for all is the max min pval, so we are good
+summary(acrossStationSameVarResults$minPval) ## 0.005376  for all is the max min pval, so we are good
 summary(acrossStationDiffVarResults$minPval)
 summary(withinStationSameVarResults$minPval)
 summary(withinStationDiffVarResults$minPval)
 
-length(which(acrossStationSameVarResults$pVal<0.05)) ## 40
+length(which(acrossStationSameVarResults$pVal<0.05)) ## 20
 nrow(acrossStationSameVarResults) ## 100
-length(which(acrossStationDiffVarResults$pVal<0.05)) ## 80
+length(which(acrossStationDiffVarResults$pVal<0.05)) ## 78
 nrow(acrossStationDiffVarResults) ## 400
-length(which(withinStationSameVarResults$pVal<0.05)) ## 7
+length(which(withinStationSameVarResults$pVal<0.05)) ## 1
 nrow(withinStationSameVarResults) ## 25
-length(which(withinStationDiffVarResults$pVal<0.05)) ## 10
+length(which(withinStationDiffVarResults$pVal<0.05)) ## 26
 nrow(withinStationDiffVarResults) ## 100
 
 sum(p.adjust(acrossStationSameVarResults$pVal, method = "BY") <0.05)## corrected p-val
-## 17
+## 5
 
 sigToPlot=acrossStationSameVar[which(p.adjust(acrossStationSameVarResults$pVal, method = "BY") <0.05),]
 resToPlot=acrossStationSameVarResults[which(p.adjust(acrossStationSameVarResults$pVal, method = "BY") <0.05),]
 
 lonSub=c(D4$Longitude[1],D10$Longitude[1],D12$Longitude[1],D22$Longitude[1],D26$Longitude[1])
 latSub=c(D4$Latitude[1], D10$Latitude[1], D12$Latitude[1],D22$Latitude[1],D26$Latitude[1])
-names(lonSub)=names(latSub)=stationNameSub
 stationNameSub=c("D4","D10","D12","D22","D26")
+names(lonSub)=names(latSub)=stationNameSub
 
 longitude=c(-121.2647, -121.5205, -121.9183, -121.8063, -121.6148, -121.7391, -121.5669, -121.5730, -121.8205,
             -122.3729, -122.1177,-122.0397, -121.9900, -121.4199, -121.3823)
@@ -47,17 +47,17 @@ text(lonSub,latSub,stationNameSub)
 sigToPlot$station1=as.character(sigToPlot$station1)
 sigToPlot$station2=as.character(sigToPlot$station2)
 
+## none for chl
+# plot(longitude,latitude,main="chl across stations")
+# points(lonSub,latSub,col="red",pch=19)
+# arrows(lonSub[sigToPlot$station1[which(sigToPlot$var1=="chl")]],latSub[sigToPlot$station1[which(sigToPlot$var1=="chl")]],
+#        lonSub[sigToPlot$station2[which(sigToPlot$var1=="chl")]],latSub[sigToPlot$station2[which(sigToPlot$var1=="chl")]],length=0)
+# for(i in 1:length(which(sigToPlot$var1=="chl"))){
+#   text(lonSub[sigToPlot$station1[which(sigToPlot$var1=="chl")]][i]+par("cxy")[2]/2,
+#        latSub[sigToPlot$station1[which(sigToPlot$var1=="chl")]][i]+par("cxy")[2]/2,resToPlot$maxLag[which(sigToPlot$var1=="chl")][i],col="dodgerblue")
+# }
 
-plot(longitude,latitude,main="chl across stations")
-points(lonSub,latSub,col="red",pch=19)
-arrows(lonSub[sigToPlot$station1[which(sigToPlot$var1=="chl")]],latSub[sigToPlot$station1[which(sigToPlot$var1=="chl")]],
-       lonSub[sigToPlot$station2[which(sigToPlot$var1=="chl")]],latSub[sigToPlot$station2[which(sigToPlot$var1=="chl")]],length=0)
-for(i in 1:length(which(sigToPlot$var1=="chl"))){
-  text(lonSub[sigToPlot$station1[which(sigToPlot$var1=="chl")]][i]+par("cxy")[2]/2,
-       latSub[sigToPlot$station1[which(sigToPlot$var1=="chl")]][i]+par("cxy")[2]/2,resToPlot$maxLag[which(sigToPlot$var1=="chl")][i],col="dodgerblue")
-}
-
-text(lonSub,latSub,stationNameSub)
+#text(lonSub,latSub,stationNameSub)
 
 plot(longitude,latitude,main="do across stations")
 points(lonSub,latSub,col="red",pch=19)
@@ -97,12 +97,10 @@ arrows(lonSub[sigToPlot$station1[which(sigToPlot$var1=="sal")]],latSub[sigToPlot
        lonSub[sigToPlot$station2[which(sigToPlot$var1=="sal")]],latSub[sigToPlot$station2[which(sigToPlot$var1=="sal")]],length=0)
 for(i in 1:length(which(sigToPlot$var1=="sal"))){
 #for(i in 1:4) { 
-  if(i==5){
-    text(-121.85,38.05,resToPlot$maxLag[which(sigToPlot$var1=="sal")][i],col="dodgerblue")
-  }else{#
+  
     text(lonSub[sigToPlot$station1[which(sigToPlot$var1=="sal")]][i]+par("cxy")[2]/2,
          latSub[sigToPlot$station1[which(sigToPlot$var1=="sal")]][i]+par("cxy")[2]/2,resToPlot$maxLag[which(sigToPlot$var1=="sal")][i],col="dodgerblue")
-  }
+  
 }
 
 text(lonSub,latSub,stationNameSub)
@@ -113,12 +111,10 @@ arrows(lonSub[sigToPlot$station1[which(sigToPlot$var1=="temp")]],latSub[sigToPlo
        lonSub[sigToPlot$station2[which(sigToPlot$var1=="temp")]],latSub[sigToPlot$station2[which(sigToPlot$var1=="temp")]],length=0)
 for(i in 1:length(which(sigToPlot$var1=="temp"))){
   #for(i in 1:4) { 
-  if(i==3){
-    text(-121.7,38.05,resToPlot$maxLag[which(sigToPlot$var1=="sal")][i],col="dodgerblue")
-  }else{#
+  
     text(lonSub[sigToPlot$station1[which(sigToPlot$var1=="temp")]][i]+par("cxy")[2]/2,
          latSub[sigToPlot$station1[which(sigToPlot$var1=="temp")]][i]+par("cxy")[2]/2,resToPlot$maxLag[which(sigToPlot$var1=="temp")][i],col="dodgerblue")
-  }
+  #}
 }
 
 text(lonSub,latSub,stationNameSub)
@@ -128,7 +124,7 @@ text(lonSub,latSub,stationNameSub)
 
 
 sum(p.adjust(acrossStationDiffVarResults$pVal, method = "BY") <0.05)## corrected p-val
-## 9
+## 7
 
 sigToPlot=acrossStationDiffVar[which(p.adjust(acrossStationDiffVarResults$pVal, method = "BY") <0.05),]
 resToPlot=acrossStationDiffVarResults[which(p.adjust(acrossStationDiffVarResults$pVal, method = "BY") <0.05),]
@@ -139,25 +135,30 @@ plot(longitude,latitude,main="across stations")
 points(lonSub,latSub,col="red",pch=19)
 arrows(lonSub[sigToPlot$station1],latSub[sigToPlot$station1],
      lonSub[sigToPlot$station2],latSub[sigToPlot$station2],length=0)
-## D4 D12 3 times
-## D4 D10
+## D4 D22 twice
+text(lonSub,latSub,stationNameSub)
+arrows(lonSub['D4'],latSub["D4"]+.01,lonSub["D22"],latSub["D22"]+.01,length=0)
+
+
 
 ## How to visualize this effectively
 ## col: nutrient 1
 ## text: nutrient 2 +lag
 ## but so much overlap
 
-
+## maybe just do table with map for reference
 
 
 sum(p.adjust(withinStationSameVarResults$pVal, method = "BY") <0.05)## corrected p-val
 ## 0
 
 sum(p.adjust(withinStationDiffVarResults$pVal, method = "BY") <0.05)## corrected p-val
-## 2
+## 4
 
 sigToPlot=withinStationDiffVar[which(p.adjust(withinStationDiffVarResults$pVal, method = "BY") <0.05),]
 resToPlot=withinStationDiffVarResults[which(p.adjust(withinStationDiffVarResults$pVal, method = "BY") <0.05),]
 
 sigToPlot
 resToPlot
+
+## table
